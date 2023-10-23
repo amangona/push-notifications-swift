453b8f2708b32f7de789a89d56e02bf7e025287a
Abe <abemangona@gmail.com>
2023-10-23 09:12:12 -0500

    remove platform specification

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@
 import PackageDescription
 
 let package = Package(name: "PushNotifications",
-                      platforms: [.macOS(.v10_10),
-                                  .iOS(.v10)],
                       products: [
                         .library(name: "PushNotifications",
                                  targets: ["PushNotifications"])
